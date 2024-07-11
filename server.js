9f3788d5d860a0a87642a97a401f1803bb4dd35a
Максим <Maksim Efimov>
2024-07-11 23:46:01 +0300

    multifiles compress

--- server.js
+++ server.js
@@ -1,74 +1,125 @@
-const express = require("express"); // Подключаем Express.js для создания сервера
-const multer = require("multer"); // Подключаем Multer для обработки файлов, загружаемых на сервер
-const { exec } = require("child_process"); // Подключаем модуль child_process для выполнения команд в терминале
-const fs = require("fs"); // Подключаем модуль fs для работы с файловой системой
-const path = require("path"); // Подключаем модуль path для работы с путями к файлам и директориям
-const cors = require("cors"); // Подключаем CORS для обеспечения работы с клиентами на другом домене
+const express = require("express");
+const multer = require("multer");
+const { exec } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
+const archiver = require("archiver");
+const { v4: uuidv4 } = require("uuid");
 
-const app = express(); // Создаем экземпляр приложения Express
-app.use(cors()); // Используем CORS middleware для разрешения CORS запросов
+const app = express();
+app.use(cors());
 
-const upload = multer({ dest: "uploads/" }); // Настраиваем Multer для временного сохранения загруженных файлов в 'uploads/' директорию
+const upload = multer({ dest: "uploads/" });
 
-app.post("/compress", upload.single("image"), (req, res) => {
-  // Обработчик POST запроса на /compress
+app.post("/compress", upload.array("images", 10), (req, res) => {
+  const outputDir = "compressed_images";
 
-  const inputPath = req.file.path; // Получаем путь к загруженному файлу из запроса
-  const outputDir = "compressed_images"; // Указываем директорию для сохранения сжатых изображений
-  const outputPath = path.join(outputDir, `${req.file.filename}.avif`); // Формируем путь к выходному файлу AVIF
-
-  // Создаем выходную директорию, если она не существует
   if (!fs.existsSync(outputDir)) {
     fs.mkdirSync(outputDir);
   }
 
-  const avifCommand = `avif --input="${inputPath}" --output="${outputDir}/" --append-ext`;
+  const inputFiles = req.files;
+  const outputFiles = [];
 
-  console.log(`Executing command: ${avifCommand}`);
+  let processedCount = 0; // Счетчик обработанных файлов
 
-  exec(avifCommand, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error: ${error.message}`);
-      return res.status(500).send("Error compressing image");
-    }
+  inputFiles.forEach((file) => {
+    const inputPath = file.path; // Путь к временному файлу
+    const outputFileName = `${file.filename}.avif`; // Имя сжатого файла
+    const outputPath = path.join(outputDir, outputFileName); // Полный путь к сжатому файлу
 
-    if (stderr) {
-      console.error(`Stderr: ${stderr}`);
-    }
+    outputFiles.push(outputPath); // Добавляем путь к сжатому файлу в массив
 
-    console.log(`Stdout: ${stdout}`);
+    // Команда для выполнения сжатия с использованием avif
+    const avifCommand = `avif --input="${inputPath}" --output="${outputDir}/" --append-ext --overwrite`;
 
-    fs.access(outputPath, fs.constants.F_OK, (err) => {
-      // Проверяем, существует ли конечный файл .avif
+    console.log(`Executing command: ${avifCommand}`);
 
-      if (err) {
-        // Если конечный файл не найден
-        console.error(`File not found: ${outputPath}`); // Выводим сообщение об ошибке в консоль
-        console.error(`Possible issues with avif CLI execution`); // Выводим дополнительную информацию для отладки
-        return res.status(404).send("Compressed image not found"); // Отправляем клиенту статус 404 и сообщение об ошибке
+    // Выполняем команду сжатия
+    exec(avifCommand, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        return res.status(500).send("Error compressing image");
       }
+      if (stderr) {
+        console.error(`Stderr: ${stderr}`);
+      }
+      console.log(`Stdout: ${stdout}`);
 
-      // Отправляем сжатый файл клиенту
-      res.sendFile(path.resolve(outputPath), (err) => {
-        if (err) {
-          // Если произошла ошибка при отправке файла клиенту
-          console.error(err); // Выводим ошибку в консоль для отладки
-          res.status(500).send("Error sending compressed image"); // Отправляем клиенту статус 500 и сообщение об ошибке
-        }
+      processedCount++;
 
-        // Удаляем временные файлы
-        fs.unlink(inputPath, (err) => {
-          if (err) console.error(err); // Выводим ошибку удаления в консоль для отладки
+      // Если все файлы обработаны
+      if (processedCount === inputFiles.length) {
+        const zipFilename = `compressed_images-${uuidv4()}.zip`; // Уникальное имя для архива
+        const zipPath = path.join(outputDir, zipFilename); // Путь к архиву
+
+        const output = fs.createWriteStream(zipPath); // Создаем поток для записи архива
+        const archive = archiver("zip", {
+          zlib: { level: 9 }, // Уровень сжатия
         });
-        fs.unlink(outputPath, (err) => {
-          if (err) console.error(err); // Выводим ошибку удаления в консоль для отладки
+
+        output.on("close", () => {
+          // Когда архивация завершена
+
+          console.log(`${archive.pointer()} total bytes`);
+          console.log("Archiver has been finalized and the output file descriptor has closed.");
+
+          // Отправляем архив клиенту для скачивания
+          res.download(zipPath, (err) => {
+            if (err) {
+              console.error(err);
+              return res.status(500).send("Error sending compressed archive");
+            }
+
+            // Удаляем временные файлы
+            inputFiles.forEach((file) => {
+              fs.unlink(file.path, (err) => {
+                if (err) console.error(err);
+              });
+            });
+            outputFiles.forEach((file) => {
+              fs.unlink(file, (err) => {
+                if (err) console.error(err);
+              });
+            });
+
+            // Удаляем временный архив после отправки клиенту
+            fs.unlink(zipPath, (err) => {
+              if (err) console.error(`Failed to delete archive: ${err}`);
+              else console.log(`Deleted archive: ${zipPath}`);
+            });
+          });
         });
-      });
+
+        output.on("end", () => {
+          console.log("Data has been drained");
+        });
+
+        archive.on("warning", (err) => {
+          if (err.code !== "ENOENT") {
+            throw err;
+          }
+        });
+
+        archive.on("error", (err) => {
+          throw err;
+        });
+
+        archive.pipe(output); // Подключаем поток архивации к потоку вывода
+
+        // Добавляем все сжатые файлы в архив
+        outputFiles.forEach((file) => {
+          archive.file(file, { name: path.basename(file) });
+        });
+
+        archive.finalize(); // Завершаем процесс архивации
+      }
     });
   });
 });
 
-const PORT = process.env.PORT || 8000; // Задаем порт для сервера
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`); // Выводим сообщение в консоль при запуске сервера
+  console.log(`Server is running on port ${PORT}`);
 });
